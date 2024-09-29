3bf662ff6c1c6181d05d4289dcd3b985d7756b30
afghansa106 <afghansa106@gmail.com>
2024-09-29 12:28:25 -0700

    create the router in the index file and make them convinient for accepting the value

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import "./App.css";
 import Header from './components/header';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './components/home';
+import Footer from './components/footer';
 
 function App() {
     return (
         <>
             <Router>
                 <Header />
+                <Home/>
+                <Footer/>
                 {/* Other components/routes can be added here */}
             </Router>
         </>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,47 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout'
+import Home from './components/home'
+import About from './components/about'
+import Contact from './components/contact'
+import Github from './components/github'
+
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      },
+      {
+        path:"github",
+        element:<Github/>
+      },
+    ]
+    
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+<RouterProvider router={router}/>
+
+
+    {/* <App /> */}
   </React.StrictMode>
 )
