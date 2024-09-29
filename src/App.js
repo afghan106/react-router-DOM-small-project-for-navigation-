406263ad57c815123d61f575bfe58992bb5992ea
afghansa106 <afghansa106@gmail.com>
2024-09-29 00:59:07 -0700

    initial commit and configuration of tailwind css and create  the git ignore file for react file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./App.css"
+import Header from './components/header';
+function App() {
+  return <>
+  <h1>wellcome to the server</h1>
+  <Header/>
+  </>
+}
+
+export default App;
