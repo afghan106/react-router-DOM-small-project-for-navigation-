971a57aca61f7e4fdd62f7d84fe8e0740e7d27b9
afghansa106 <afghansa106@gmail.com>
2024-09-29 11:49:44 -0700

    change to the header file while it was not working properly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import "./App.css"
+import "./App.css";
 import Header from './components/header';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 function App() {
-  return <>
-  <h1>wellcome to the server</h1>
-  <Header/>
-  </>
+    return (
+        <>
+            <Router>
+                <Header />
+                {/* Other components/routes can be added here */}
+            </Router>
+        </>
+    );
 }
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
     return (
@@ -7,35 +7,30 @@ export default function Header() {
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
-                        <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                            className="mr-3 h-12"
-                            alt="Logo"
-                        />
+                        <h1 className='inline-block text-orange-500 text-5xl font-bold'>LO<span className='text-black'>G</span>O</h1>
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <Link
-                            to="#"
+                            to="/login" 
                             className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            aria-label="Log in"
                         >
                             Log in
                         </Link>
                         <Link
-                            to="#"
+                            to="/signup" // Use a valid route for getting started
                             className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            aria-label="Get started"
                         >
                             Get started
                         </Link>
                     </div>
-                    <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                        id="mobile-menu-2"
-                    >
+                    <div className="flex justify-between items-center w-full lg:w-auto lg:order-1">
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                to="/"
-                                    className={({isActive}) =>
+                                    to="/"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -44,8 +39,8 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                to="/about"
-                                    className={({isActive}) =>
+                                    to="/about"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -54,8 +49,8 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                to="/contact"
-                                    className={({isActive}) =>
+                                    to="/contact"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -64,15 +59,14 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                to="/github"
-                                    className={({isActive}) =>
+                                    to="/github"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Github
                                 </NavLink>
                             </li>
-                            
                         </ul>
                     </div>
                 </div>
